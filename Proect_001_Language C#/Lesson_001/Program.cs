35c51d4dc7f1ec8955deeef8aa6108b3bc3d69f5
Andrey Vavilov <st.bredlam@gmail.com>
2023-04-21 23:16:49 +0300

    1 пример с 1-го семинара

--- Proect_001_Language C#/Lesson_001/Program.cs
+++ Proect_001_Language C#/Lesson_001/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое число (тип Строка): ");
+string UserInput = Console.ReadLine() ?? "";
+int UserNumber = int.Parse(UserInput);
+//int UserNumber = int.Parse(Console.ReadLine() ?? "";); - вариант когда мы в качестве параметра для метода .Parse() сразу используем метод .Readlile() 
+int Result = UserNumber * UserNumber;
+Console.WriteLine("Вывод квадрат числа преобразованных данных (тип Число) = " + Result);
+Console.WriteLine(UserInput + " -> " + Result);
+Console.WriteLine($"{UserInput} -> {Result}"); // метод интерполяции. Метод позволяющий использовать переменные внутри строки
