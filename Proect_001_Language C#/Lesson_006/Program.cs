3f670b6edcef8cfc5683a4b7650ec69235035954
Andrey Vavilov <st.bredlam@gmail.com>
2023-04-21 20:00:59 +0300

    написал алгоритм поиска маскимального числа

--- Proect_001_Language C#/Lesson_006/Program.cs
+++ Proect_001_Language C#/Lesson_006/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 12;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (max < a) max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+
+Console.Write("Мах равен = ");
+Console.WriteLine(max);
