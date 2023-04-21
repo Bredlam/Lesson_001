570a59630eac1be302a55ce1a31b285b252a8ab6
Andrey Vavilov <st.bredlam@gmail.com>
2023-04-22 00:10:02 +0300

    3 задачи записал с семинара Урок 1. Знакомство с языком программирования С#

--- Proect_001_Language C#/Lesson_001/Program.cs
+++ Proect_001_Language C#/Lesson_001/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write("Введите целое число (тип Строка): ");
+﻿Console.Clear();
+// Урок разбирал препод - я писал за ним
+Console.WriteLine("Задача №1 ");
+Console.Write("Введите целое число (тип Строка): ");
 string UserInput = Console.ReadLine() ?? "";
 int UserNumber = int.Parse(UserInput);
 //int UserNumber = int.Parse(Console.ReadLine() ?? "";); - вариант когда мы в качестве параметра для метода .Parse() сразу используем метод .Readlile() 
@@ -6,3 +9,44 @@ int Result = UserNumber * UserNumber;
 Console.WriteLine("Вывод квадрат числа преобразованных данных (тип Число) = " + Result);
 Console.WriteLine(UserInput + " -> " + Result);
 Console.WriteLine($"{UserInput} -> {Result}"); // метод интерполяции. Метод позволяющий использовать переменные внутри строки
+
+// Напишите программу, которая на вход принимает два числа 
+// и проверяет, является ли первое число квадратом второго.
+Console.WriteLine(" ");
+Console.WriteLine("Задача №2 ");
+Console.Write("Введите 1-е число - ");
+int UserInputA = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите 2-е число - ");
+int UserInputB = int.Parse(Console.ReadLine() ?? "");
+
+int Result1 = UserInputA / UserInputB;
+if (Result1 == UserInputB)
+{
+   Console.WriteLine("Да"); 
+}
+else
+{
+    Console.WriteLine("Нет"); 
+}
+
+// Напишите программу, которая будет выдавать название
+// дня недели по заданному номеру за интервал номеров
+// дней недели заступать нельзя
+Console.WriteLine(" ");
+Console.WriteLine("Задача №3 ");
+Console.Write("Введите день недели ");
+int UserInputС = int.Parse(Console.ReadLine() ?? "");
+if (UserInputС > 0 & UserInputС < 8)
+{
+   if (UserInputС == 1) Console.Write("Понедельник");
+   if (UserInputС == 2) Console.Write("Вторник");
+   if (UserInputС == 3) Console.Write("Среда");
+   if (UserInputС == 4) Console.Write("Четверг");
+   if (UserInputС == 5) Console.Write("Пятница");
+   if (UserInputС == 6) Console.Write("Суббота");
+   if (UserInputС == 7) Console.Write("Воскресенье");    
+}
+else
+{
+    Console.Write("Вы ввели неверные данные");
+}
--- Proect_001_Language C#/Lesson_002/Program.cs
+++ Proect_001_Language C#/Lesson_002/Program.cs
@@ -1,4 +1,37 @@
-﻿Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿// Напишите программу, которая на вход принимает два числа 
+// и проверяет, является ли первое число квадратом второго.
+Console.Write("Введите 1-е число - ");
+int UserInputA = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите 2-е число - ");
+int UserInputB = int.Parse(Console.ReadLine() ?? "");
+
+int Result = UserInputA / UserInputB;
+if (Result == UserInputB)
+{
+   Console.WriteLine("Да"); 
+}
+else
+{
+    Console.WriteLine("Нет"); 
+}
+
+// Напишите программу, которая будет выдавать название
+// дня недели по заданному номеру за интервал номеров
+// дней недели заступать нельзя
+
+Console.Write("Введите день недели ");
+int UserInput = int.Parse(Console.ReadLine() ?? "");
+if (UserInput > 0 & UserInput < 8)
+{
+   if (UserInput == 1) Console.Write("Понедельник");
+   if (UserInput == 2) Console.Write("Вторник");
+   if (UserInput == 3) Console.Write("Среда");
+   if (UserInput == 4) Console.Write("Четверг");
+   if (UserInput == 5) Console.Write("Пятница");
+   if (UserInput == 1) Console.Write("Суббота");
+   if (UserInput == 1) Console.Write("Воскресенье");    
+}
+else
+{
+    Console.Write("Вы ввели неверные данные");
+}
