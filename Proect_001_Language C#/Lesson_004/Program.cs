b8222ece703a63b92eef8b9490763f6fb07324f5
Andrey Vavilov <st.bredlam@gmail.com>
2023-04-22 14:33:47 +0300

    ДЗ

--- Proect_001_Language C#/Lesson_004/Program.cs
+++ Proect_001_Language C#/Lesson_004/Program.cs
@@ -1,3 +1,20 @@
-﻿double numderA = 12;
-double numderB = 5;
-Console.WriteLine(numderA / numderB);
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+string FirstInput = Console.ReadLine() ?? "";
+int FirstInt = int.Parse(FirstInput);
+
+Console.Write("Введите второе число: ");
+string SecondInput = Console.ReadLine() ?? "";
+int SecondInt = int.Parse(SecondInput);
+
+Console.Write("Введите третье число: ");
+string ThirdInput = Console.ReadLine() ?? "";
+int ThirdInt = int.Parse(ThirdInput);
+
+int MaxInt = FirstInt;
+if (MaxInt < SecondInt) MaxInt = SecondInt;
+if (MaxInt < ThirdInt) MaxInt = ThirdInt;
+
+Console.Write($"Максимальное число = {MaxInt}");
