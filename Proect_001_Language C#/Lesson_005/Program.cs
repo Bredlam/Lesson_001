52d23b3ee6312d7e978e0b7e1c7c8a90e12414fe
Andrey Vavilov <st.bredlam@gmail.com>
2023-04-22 19:17:02 +0300

    коментарий

--- Proect_001_Language C#/Lesson_005/Program.cs
+++ Proect_001_Language C#/Lesson_005/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Введите имя: ");
+﻿// Используем метод .ToLower() нижнего регистра для унифицации строковых данных во избежание невозможности сравнить
+// данные из-за разных регистров (верхних и нижних) строковых данных
+Console.Write("Введите имя: ");
 string UserName = Console.ReadLine() ?? "";
 string ControlName = "Андрей";
 if (ControlName.ToLower() == UserName.ToLower())
