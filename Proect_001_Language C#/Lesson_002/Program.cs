11822d4334162565729b014f17fc293137524261
Andrey Vavilov <st.bredlam@gmail.com>
2023-04-22 14:17:53 +0300

    ДЗ

--- Proect_001_Language C#/Lesson_002/Program.cs
+++ Proect_001_Language C#/Lesson_002/Program.cs
@@ -1,37 +1,15 @@
-﻿// Напишите программу, которая на вход принимает два числа 
-// и проверяет, является ли первое число квадратом второго.
-Console.Write("Введите 1-е число - ");
-int UserInputA = int.Parse(Console.ReadLine() ?? "");
-Console.Write("Введите 2-е число - ");
-int UserInputB = int.Parse(Console.ReadLine() ?? "");
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int FirstInt = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите второе число: ");
+int SecondtInt = int.Parse(Console.ReadLine() ?? "");
 
-int Result = UserInputA / UserInputB;
-if (Result == UserInputB)
+if (FirstInt > SecondtInt)
 {
-   Console.WriteLine("Да"); 
+    Console.Write($"max = {FirstInt}");
 }
 else
 {
-    Console.WriteLine("Нет"); 
-}
-
-// Напишите программу, которая будет выдавать название
-// дня недели по заданному номеру за интервал номеров
-// дней недели заступать нельзя
-
-Console.Write("Введите день недели ");
-int UserInput = int.Parse(Console.ReadLine() ?? "");
-if (UserInput > 0 & UserInput < 8)
-{
-   if (UserInput == 1) Console.Write("Понедельник");
-   if (UserInput == 2) Console.Write("Вторник");
-   if (UserInput == 3) Console.Write("Среда");
-   if (UserInput == 4) Console.Write("Четверг");
-   if (UserInput == 5) Console.Write("Пятница");
-   if (UserInput == 1) Console.Write("Суббота");
-   if (UserInput == 1) Console.Write("Воскресенье");    
-}
-else
-{
-    Console.Write("Вы ввели неверные данные");
+    Console.Write($"max = {SecondtInt}");
 }
