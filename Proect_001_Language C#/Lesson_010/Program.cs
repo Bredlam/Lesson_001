758f7c82b6d753c7bffec31a5b1b195361cf5a05
Andrey Vavilov <st.bredlam@gmail.com>
2023-04-23 18:23:16 +0300

    добавлены новые уроки Lesson_010,_013,_015

--- Proect_001_Language C#/Lesson_010/Program.cs
+++ Proect_001_Language C#/Lesson_010/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+// Например: 456 -> 5 ... 782 -> 8 ... 917 -> 1
+Console.Clear();
+int UserInt;
+Console.Write("Введите целое трехзначное число: ");
+while (true)
+{
+    if ((int.TryParse(Console.ReadLine(), out UserInt)))
+    {
+        int ControlInt = 0;
+        int coint = UserInt;
+        while (coint > 0)
+        {
+            ControlInt++;
+            coint/=10;
+        }
+        if (ControlInt == 3) break;        
+    }
+    Console.WriteLine("Ошибка. Введите целое трехзначное число!");
+}
+
+int OstInt = UserInt % 100 / 10;
+
+Console.WriteLine($"Результат: {OstInt}");
--- Proect_001_Language C#/Lesson_013/Program.cs
+++ Proect_001_Language C#/Lesson_013/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Например: 645 -> 5 ... 78 -> третьей цифры нет ... 32679 -> 6
+Console.Clear();
+int UserInt;
+Console.Write("Введите целое число: ");
+while (true) // Проверка на ввод целого числа
+{
+    if ((int.TryParse(Console.ReadLine(), out UserInt)))
+    {
+        break;
+    }
+    Console.Write("Ошибка. Введите целое число!:");
+}
+
+int Сount = 0;
+int TmpInt01 = UserInt;
+while (TmpInt01 > 0) // Вычисляем сколько знаков в числе
+{
+    Сount++;
+    TmpInt01 /= 10;
+}
+
+if (Сount >= 3) // Вычисляем 3-ю цифру если число хотя бы трехзначное
+{
+    int TmpInt02 = UserInt;
+    while (TmpInt02 >= 1000)
+    {
+        TmpInt02 /= 10;
+    }
+    int OstInt = TmpInt02 % 10;
+
+    Console.WriteLine($"{UserInt} -> {OstInt}");
+}
+else // Отказ
+{
+    Console.WriteLine($"{UserInt} -> третьей цифры нет");
+}
