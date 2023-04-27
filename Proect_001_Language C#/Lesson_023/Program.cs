9b823afb1317ba4b949f832f19be85847052c786
Andrey Vavilov <st.bredlam@gmail.com>
2023-04-27 22:01:47 +0300

    ДЗ № 23

--- Proect_001_Language C#/Lesson_023/Program.cs
+++ Proect_001_Language C#/Lesson_023/Program.cs
@@ -3,6 +3,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N
+        // Например: 5 -> 1, 8, 27, 64, 125
+
+        int userNum = ControlUserInput(); // Вызов метода
+        UserCalculations(userNum);
+
+        static int ControlUserInput() // Определение метода - 
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите число: ");
+                    int num = int.Parse(Console.ReadLine() ?? "");
+                    return num;
+                }
+                catch (System.Exception exc)
+                {
+                    Console.WriteLine($"Ошибка! Введите число: {exc.Message}");
+                }
+            }
+        }
+        static void UserCalculations(int num) // Определение метода - 
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                int result = Convert.ToInt32(Math.Pow(i,3));
+                Console.Write($"{result}, ");
+            }
+        }
     }
 }
