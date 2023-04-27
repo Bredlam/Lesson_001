686126db20a28ff967f2ee6eb3f8dc33b952ebd2
Andrey Vavilov <st.bredlam@gmail.com>
2023-04-27 22:03:08 +0300

    ДЗ № 21

--- Proect_001_Language C#/Lesson_021/Program.cs
+++ Proect_001_Language C#/Lesson_021/Program.cs
@@ -6,22 +6,22 @@ class Program
         // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
         // Например: А(3, 6, 8) / В(2, 1, -7) -> 15,84
         Console.Clear();        
-        string[] NameCoordinates = {"x1","y1","z1","x2","y2","z2"};
-        double[] Coordinates = {0,0,0,0,0,0};
+        string[] nameCoordinates = {"x1","y1","z1","x2","y2","z2"};
+        double[] coordinates = {0,0,0,0,0,0};
 
-        UserControlInput(ref Coordinates, NameCoordinates); // Вызов метода
-        CalculatingLengthVector(Coordinates); // Вызов метода
+        UserControlInput(ref coordinates, nameCoordinates); // Вызов метода
+        CalculatingLengthVector(coordinates); // Вызов метода
 
-        static void UserControlInput(ref double[] Coord, string[] NameCoord) // Определение метода - контроль ввода данных        
+        static void UserControlInput(ref double[] coord, string[] nameCoord) // Определение метода - контроль ввода данных        
         {            
-            for (int i = 0; i < Coord.Length; i++)
+            for (int i = 0; i < coord.Length; i++)
             {
                 while (true)
                 {
                     try
                     {
-                        Console.Write($"Введите координаты {NameCoord[i]}: ");
-                        Coord[i] = double.Parse(Console.ReadLine() ?? "");
+                        Console.Write($"Введите координаты {nameCoord[i]}: ");
+                        coord[i] = double.Parse(Console.ReadLine() ?? "");
                         break;
                     }
                     catch (System.Exception exc)
@@ -32,12 +32,12 @@ class Program
             }
 
         }
-        static void CalculatingLengthVector(double[] Coord) // Определение метода - вычисления расстояния между 2-х точек в 3D пространсте
+        static void CalculatingLengthVector(double[] coord) // Определение метода - вычисления расстояния между 2-х точек в 3D пространсте
         {
             double result01 = 0;
-            for (int i = 0; i < Coord.Length-3; i++)
+            for (int i = 0; i < coord.Length-3; i++)
             {
-                double result02 = Math.Pow(Coord[i] - Coord[i+3],2);
+                double result02 = Math.Pow(coord[i] - coord[i+3],2);
                 result01  += result02;                
             }
             Console.Write($"{(Math.Sqrt(result01)):f2}");
