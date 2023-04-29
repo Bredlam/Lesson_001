ec38886823da67b3ba89780a095a5ad87c3436e6
Andrey Vavilov <st.bredlam@gmail.com>
2023-04-29 17:31:53 +0300

    добавлен теоретический материал

--- Proect_001_Language C#/Lesson_015/Program.cs
+++ Proect_001_Language C#/Lesson_015/Program.cs
@@ -1,1 +1,13 @@
 ﻿
+int number = 456789;
+int result = 0;
+int y = number.ToString().Length;
+
+for (int i = 0; i < y; i++)
+{
+    result += number % 10;
+    number /= 10;
+}
+Console.WriteLine(result);
+
+
