d38a87a81739fec7c63cdbb6fd0210a43c1075e5
Andrey Vavilov <st.bredlam@gmail.com>
2023-04-29 19:03:36 +0300

    добавлены дз

--- Proect_001_Language C#/Homework_001/Program.cs
+++ Proect_001_Language C#/Homework_001/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача. Напишите цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В.
+// Например: 3, 5 -> 243
+int[] userArray02 = ControlUserInput04();
+double userResult03 = NumPower(userArray02);
+ResultOutput04(userArray02, userResult03);
+
+int[] ControlUserInput04() // Определение метода - контроль ввода данных пользователем
+{
+    int[] array = { 0, 0 };
+    for (int i = 1; i < 3; i++)
+    {
+        while (true)
+        {
+            Console.Write($"Введите {i}-е число: ");
+            if (int.TryParse(Console.ReadLine(), out array[i - 1])) break;
+        }
+
+    }
+    return array;
+}
+double NumPower(int[] array) // Определение метода - возведение числа в степень
+{
+    double result = Math.Pow(array[0], array[1]);
+    return result;
+}
+void ResultOutput04(int[] array, double num) // Определение метода - вывод данных пользователю
+{
+    Console.WriteLine($"{array[0]}, {array[1]} -> {num}");
+}
--- Proect_001_Language C#/Homework_002/Program.cs
+++ Proect_001_Language C#/Homework_002/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача. Напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+// Например: 452 -> 11
+int ControlUserInput05() // Определение метода - контроль ввода данных пользователя
+{
+    while (true)
+    {
+        Console.Write("Введите целое число: ");
+        if (int.TryParse(Console.ReadLine(), out int userNum))
+            return userNum;
+        Console.WriteLine("Ошибка! Введите целое число: ");
+    }
+}
+int SummNumOfTally(int num) // Определение метода - вычисление суммы цифр числа
+{
+    int result = 0;
+    int numLenght = num.ToString().Length;
+    for (int i = 0; i < numLenght; i++)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result;
+}
+void ResultOutput05(int num, int result) // Определение метода - вывод данных пользователю
+{
+    System.Console.WriteLine($"{num} -> {result}");
+}
+
+int userNum03 = ControlUserInput05();
+int userResult04 = SummNumOfTally(userNum03);
+ResultOutput05(userNum03, userResult04);
